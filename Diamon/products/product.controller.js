b25ee31c1bfd8e62248aeb00ae01eb774ca4283e
luanpham <luanpham@mddi.com>
2024-01-03 22:18:06 +0700

    Update product.controller.js

--- Diamon/products/product.controller.js
+++ Diamon/products/product.controller.js
@@ -12,20 +12,6 @@ let create = async (req, res, next) => {
         .catch(next);
 }
 
-/*
-categoryId: {type: DataTypes.INTEGER, allowNull: false},
-        userId: {type: DataTypes.INTEGER, allowNull: false},
-        title: { type: DataTypes.STRING, allowNull: false },        
-        sumary: { type: DataTypes.STRING, allowNull: false },
-        price: { type: DataTypes.DOUBLE, allowNull: false },
-        discount: { type: DataTypes.DOUBLE, allowNull: false },
-        quantity: {type: DataTypes.INTEGER, allowNull: false},
-        content: { type: DataTypes.TEXT, allowNull: false },        
-        image: { type: DataTypes.STRING, allowNull: true },
-        publishedAt: {type: DataTypes.DATE, allowNull: true},
-        startAt: {type: DataTypes.INTEGER, allowNull: true},
-        endAt: {type: DataTypes.INTEGER, allowNull: true},
- */
 async function createSchema(req, res, next) {
     const schema = Joi.object({
         categoryId: Joi.number().required(),
